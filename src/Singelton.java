6c69f7af81f23e5096cdddf2fa864385bbd7aef5
BigLamer <tempandy2013@gmail.com>
2013-05-23 12:55:04 +0300

    NEW SINGELTON

--- src/Singelton.java
+++ src/Singelton.java
@@ -4,14 +4,17 @@
  *
  */
 public class Singelton {
-    private static final Singelton INSTANCE = new Singelton();
-    // Private constructor prevents instantiation from other classes
+      // Private constructor prevents instantiation from other classes
     private Singelton() {
         barans=1;
     }
-    public static Singelton getInstance() {
-        return INSTANCE;
-    }
+    private static class SingeltonHolder {
+        public static final Singelton INSTANCE = new Singelton();
+      }
+
+      public static Singelton getInstance() {
+        return SingeltonHolder.INSTANCE;
+      }
     private int barans;
     public static void main(String[] args) {
         Singelton sing=Singelton.getInstance();
